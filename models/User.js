919968a1178a2b2bf6b0d6d822f4a9462243bce5
hjohnsick <hjohnsick@yahoo.com>
2022-04-02 16:06:58 -0400

    added virtual to user schema

--- models/User.js
+++ models/User.js
@@ -1,20 +1,43 @@
 const { Schema, model } = require("mongoose");
 
-const UserSchema = new Schema({
-  username: {
-    type: String,
-    unique: true,
-    required: "Username is required!",
-    trim: true,
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: "Username is required!",
+      trim: true,
+    },
+    email: {
+      type: String,
+      unique: true,
+      required: "Email is required!",
+      match: /.+\@.+\..+/,
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
   },
-  email: {
-    type: String,
-    unique: true,
-    required: "Email is required!",
-    match: /.+\@.+\..+/,
-  },
-  thoughts: [],
-  friends: [],
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// get the total number of friends for a user
+UserSchema.virtual("friendCount").get(function () {
+  return this.thoughts.length;
 });
 
 const User = model("User", UserSchema);
