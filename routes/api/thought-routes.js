18956693d985a341f6a281788ec3e5f2280b5054
hjohnsick <hjohnsick@yahoo.com>
2022-04-03 09:15:34 -0400

    added thought routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,19 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  deleteThought,
+} = require("../../controllers/thought-controller");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(addThought);
+
+router
+  .route("/:id")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(deleteThought);
+
+module.exports = router;
